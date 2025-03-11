75b63d7097457f111a4170a4ac338ed965a00ea1
Alex S <tonezone6@gmail.com>
2025-03-11 11:34:01 +0100

    Add modifier for hiding back button title using navigation item

---
+++ NavigationBarAppearence/1-ViewExtractor/View-BackButtonMinimalModifier.swift
@@ -0,0 +1,46 @@
+import SwiftUI
+
+extension View {
+    public func navigationBackButtonMinimal() -> some View {
+        self.background(NavigationItemExtractor { navigationItem in
+            navigationItem?.backButtonDisplayMode = .minimal
+        })
+    }
+}
+
+struct NavigationItemExtractor: UIViewControllerRepresentable {
+    let result: (UINavigationItem?) -> Void
+
+    func makeUIViewController(context: Context) -> UIViewController {
+        let viewController = ViewController(result: result)
+        return viewController
+    }
+
+    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+    }
+    
+    class ViewController: UIViewController {
+        let result: (UINavigationItem?) -> Void
+
+        init(result: @escaping (UINavigationItem?) -> Void) {
+            self.result = result
+            super.init(nibName: nil, bundle: nil)
+        }
+
+        required init?(coder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+
+        override func viewWillAppear(_ animated: Bool) {
+            super.viewWillAppear(animated)
+
+            result(parent?.navigationItem)
+        }
+        
+        override func viewDidAppear(_ animated: Bool) {
+            super.viewDidAppear(animated)
+            
+            result(parent?.navigationItem)
+        }
+    }
+}
